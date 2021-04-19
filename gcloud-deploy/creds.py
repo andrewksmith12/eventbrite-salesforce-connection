b152a6517c83af10f45e22efbbd96c32f139fb87
Andrew Smith <43506065+andrewksmith12@users.noreply.github.com>
2021-04-19 12:40:11 -0500

    Create sample creds.py

---
+++ gcloud-deploy/creds.py
@@ -0,0 +1,12 @@
+# SALESFORCE CREDENTIALS
+SF_SANDBOX_URL = "xxxxx--sandbox.my.salesforce.com"
+SF_SANDBOX_USERNAME = "email@example.com.sandbox"
+SF_SANDBOX_SECURITY_TOKEN = ""
+SF_PROD_URL = "xxxxx.my.salesforce.com/"
+SF_USERNAME = "email@example.com"
+SF_PASSWORD = ""
+SF_PROD_SECURITY_TOKEN = ""
+
+#EVENTBRITE CREDENTIALS
+EB_API_KEY = ""
+EB_TEST_API_KEY = ""
