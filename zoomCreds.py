f0fc75d0cd451acac1138bee73256a1843c27f68
Andrew Smith <andrew@andrewksmith.net>
2021-04-28 23:45:34 -0500

    Update zoomCreds template to include header

--- zoomCreds.py
+++ zoomCreds.py
@@ -1,4 +1,5 @@
-API_KEY = "f9mb_3-jS4OLXCjYN1ogeA"
-API_SECRET = "i5bmKmH5lsiFW0TUJn3uXHEviORAresGKgzU"
-IM_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJSajVuX3dPM1FfaXN4Q2tVaFFFU3BRIn0.vD0mOKkptP8G8zYkwO1FYTQ3Uyyjm_IZIhY9pn75Pek"
-JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImY5bWJfMy1qUzRPTFhDallOMW9nZUEiLCJleHAiOjE2MjAxNTQ1MzksImlhdCI6MTYxOTU0OTczOX0.AUsKwqnUct0hQfwrr8lYoScvqladJsi9Ozjy0bi6YFk"
+# Example Zoom Credentials
+API_KEY = "fewrasefesrtredgredgfv"
+API_SECRET = "i5bmKmH5lsfgdfssgserdw4w23sGKgzU"
+IM_TOKEN = "ehgvrdehnfpuiehaorudhw.ksajrfknbiouwherkjodghaeoiurf.riausrfhoqiu4wgh74y7"
+JWT = "fojjnfkljehfopuihjwekjrthfoiuesrhir.9872485y34uheur7498rheiuhrt8734yueihwr87f4y3uheut34t3frfgd.dgpiejrsdiughkljsdhzfd;i"
